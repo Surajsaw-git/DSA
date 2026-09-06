c2fe290a95a04dab18242bb0f2d61433bcd6ffa8
Surajsaw-git <surajsaw7050500@gmail.com>
2026-09-06 23:17:23 +0530

    Code Added second max

---
+++ CPP/Code6.cpp
@@ -0,0 +1,37 @@
+//Find the second largest element in an array without sorting.
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+vector<int> arr{10,10,5,10,10};
+int i;
+for(i =0; i<arr.size();i++){
+    cout<<arr[i]<<" ";
+}
+cout<<endl;
+
+
+int temp =arr[0];
+int second_max=arr[0];
+
+for(i =1; i<arr.size();i++)
+{
+    if( temp<arr[i]){
+        second_max=temp;
+        temp = arr[i];
+    }
+    else if(second_max < arr[i] && arr[i] < temp)
+    {
+        second_max = arr[i];
+
+    }
+    
+
+}
+cout<<temp<<endl;
+
+cout<<second_max<<endl;
+
+
+    return 0;
+}
